9bdc64585873c00d6543edad5aad3ffdf9b2ca6e
Varun Gangipelli <varunkumargangipelli@gmail.com>
2024-11-28 23:42:03 +0530

    files uploaded

---
+++ routes/user.js
@@ -0,0 +1,67 @@
+const { Router } = require("express");
+const router = Router();
+const userMiddleware = require("../middleware/user");
+const { Admin, User, Course } = require("../db");
+const {JWT_SECRET} = require("../config");
+const jwt = require("jsonwebtoken");
+
+// User Routes
+router.post('/signup', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // check if a user with this username already exists
+    await User.create({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        message: 'User created successfully'
+    })
+});
+
+router.post('/signin', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log(JWT_SECRET);
+
+    const user = await User.find({
+        username,
+        password
+    })
+    if (user) {
+        const token = jwt.sign({
+            username
+        }, JWT_SECRET);
+
+        res.json({
+            token
+        })
+    } else {
+        res.status(411).json({
+            message: "Incorrect email and pass"
+        })
+    }
+});
+
+router.get('/courses', async (req, res) => {
+    const response = await Course.find({});
+
+    res.json({
+        courses: response
+    })
+});
+
+router.post('/courses/:courseId', userMiddleware, (req, res) => {
+    // Implement course purchase logic
+    const username = req.username;
+    console.log(username);
+
+});
+
+router.get('/purchasedCourses', userMiddleware, (req, res) => {
+    // Implement fetching purchased courses logic
+});
+
+module.exports = router
